760414f480c5033d75a8c8f8dc7c6a463d3499fe
Maksim Oskolskii <oskolsky@CY1-AL-298.corp.wargaming.local>
2025-06-17 11:44:03 +0300

    Change footer

--- src/components/features/location-picker/location-picker.tsx
+++ src/components/features/location-picker/location-picker.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react'
 
+import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 import { config } from '@/utils/config'
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     return (
         <footer className="flex flex-col justify-between border-t border-gray-200 py-4 text-sm text-gray-500 sm:flex-row">
             <div>
-                &copy; 2025 Geo tools for{' '}
+                &copy; 2025 Location Picker by{' '}
                 <Link
                     href="https://www.tripadvancer.com"
                     target="__blank"

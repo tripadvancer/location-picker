4f9bb3f11a67563763987436eb133fb408f20e6a
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-29 23:11:51 +0300

    Fix texts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,14 @@
+import { Metadata } from 'next'
+
 import { About } from '@/components/features/about/about'
+
+// prettier-ignore
+export const metadata: Metadata = {
+    title: 'About Location Picker',
+    openGraph: {
+        title: 'About Location Picker',
+    },
+}
 
 export default function AboutPage() {
     return <About />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,12 +28,13 @@ export const metadata: Metadata = {
         canonical: '/',
     },
     title: 'Location Picker - Search, Convert, and Share Locations',
-    description: 'Search locations by coordinates or address. Convert between DD and DMS. Share via Google Maps, Waze, Apple or Yandex. Easy, accurate, and free.',
+    description: 'Location Picker is a free online tool for searching, converting, and sharing locations by address or coordinates. Supports DD and DMS formats, and integration with Google Maps, Waze, Apple Maps, and Yandex.Maps.',
     appleWebApp: {
         title: 'Location Picker',
     },
     openGraph: {
         title: 'Location Picker - Search, Convert, and Share Locations',
+        description: 'Location Picker is a free online tool for searching, converting, and sharing locations by address or coordinates. Supports DD and DMS formats, and integration with Google Maps, Waze, Apple Maps, and Yandex.Maps.',
         type: 'website',
         locale: 'ru_RU',
         url: '/',
@@ -41,6 +42,7 @@ export const metadata: Metadata = {
     },
     twitter: {
         title: 'Location Picker - Search, Convert, and Share Locations',
+        description: 'Location Picker is a free online tool for searching, converting, and sharing locations by address or coordinates. Supports DD and DMS formats, and integration with Google Maps, Waze, Apple Maps, and Yandex.Maps.',
         card: 'summary_large_image',
     },
 }

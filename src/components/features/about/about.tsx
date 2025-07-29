1466b53d7594bc323df68eaee49ac830dbee6725
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-29 23:04:47 +0300

    Fix

--- src/components/features/about/about.tsx
+++ src/components/features/about/about.tsx
@@ -5,17 +5,17 @@ import { AboutExpander } from './components/about-expander'
 export const About = () => {
     return (
         <div className="space-y-4">
-            <p className="mb-4">
-                <strong>Location Picker</strong> is a convenient online tool for searching, converting, and sharing
-                coordinates. You can search for places by address or coordinates, easily convert between DD and DMS
-                formats, and open or share any location via Google Maps, Waze, Apple Maps, or Yandex.Maps. A simple,
-                accurate, and free solution for anyone working with geolocation.
+            <p className="mb-8">
+                <strong>Location Picker</strong> is&nbsp;a&nbsp;convenient online tool for searching, converting, and
+                sharing coordinates. You can search for places by&nbsp;address or&nbsp;coordinates, easily convert
+                between&nbsp;DD and DMS formats, and open or&nbsp;share any location via Google Maps, Waze, Apple Maps,
+                or&nbsp;Yandex.Maps. A&nbsp;simple, accurate, and free solution for anyone working with geolocation.
             </p>
 
             <AboutExpander title="Use in Full Screen Mode on iPhone and Android">
                 <p className="mb-4">
-                    For the best experience, launch this app directly from your home screen to enjoy full-screen mode on
-                    your device.
+                    For the best experience, launch this app directly from your home screen to&nbsp;enjoy full-screen
+                    mode on your device.
                 </p>
 
                 <h4 className="mb-2 font-bold">How to Install on iPhone</h4>
@@ -31,9 +31,9 @@ export const About = () => {
                         </Link>
                         .
                     </li>
-                    <li>Tap the "Share" button.</li>
-                    <li>Select "Add to Home Screen".</li>
-                    <li>Tap the Location Picker icon on your home screen to launch the app.</li>
+                    <li>Tap the &laquo;Share&raquo; button.</li>
+                    <li>Select &laquo;Add to&nbsp;Home Screen&raquo;.</li>
+                    <li>Tap the Location Picker icon on&nbsp;your home screen to&nbsp;launch the app.</li>
                 </ol>
 
                 <h4 className="mb-2 font-bold">How to Install on Android</h4>
@@ -49,9 +49,9 @@ export const About = () => {
                         </Link>
                         .
                     </li>
-                    <li>Tap the menu button (three dots) in the top-right corner.</li>
-                    <li>Select "Add to Home Screen".</li>
-                    <li>Tap the Location Picker icon on your home screen to launch the app.</li>
+                    <li>Tap the menu button (three dots) in&nbsp;the top-right corner.</li>
+                    <li>Select &laquo;Add to&nbsp;Home Screen&raquo;.</li>
+                    <li>Tap the Location Picker icon on&nbsp;your home screen to&nbsp;launch the app.</li>
                 </ol>
             </AboutExpander>
         </div>

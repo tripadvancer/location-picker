260afbd5756863151e3fe148643798c4b05d8479
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2025-07-30 18:30:07 +0300

    New logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,12 +1,15 @@
+import { MapPinCheckIcon } from 'lucide-react'
+
 import Image from 'next/image'
 import Link from 'next/link'
 
 export const Logo = () => {
     return (
-        <Link href="/" className="flex items-center gap-x-2">
-            <Image src="/images/logo.svg" width="140" height="24" alt="Tripadvancer" />
-            <span>|</span>
-            <span className="text-sm font-semibold whitespace-nowrap uppercase">Location Picker</span>
+        <Link href="/" className="flex w-full items-center gap-x-2 sm:w-auto">
+            {/* <Image src="/images/logo.svg" width="140" height="24" alt="Tripadvancer" /> */}
+            {/* <span>|</span> */}
+            <MapPinCheckIcon strokeWidth={2.5} />
+            <span className="text-xl font-extrabold whitespace-nowrap uppercase">Location Picker</span>
         </Link>
     )
 }

2b489f65ed42c78e7e517d5f7a5da67dc26d0e1d
Maxim Oskolsky <oskolsky.maxim@gmail.com>
2026-01-16 14:47:41 +0200

    PWA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,22 @@ export const metadata: Metadata = {
     alternates: {
         canonical: '/',
     },
-    title: 'Location Picker - Search, Convert, and Share Locations',
-    description: 'Location Picker is a free online tool for searching, converting, and sharing locations by address or coordinates. Supports DD and DMS formats, and integration with Google Maps, Waze, Apple Maps, and Yandex.Maps.',
+    title: 'Location Picker - Store and Share Locations',
+    description: 'Location Picker is a free online tool for storing and sharing locations by address or coordinates. Supports DD and DMS formats, and integration with Google Maps, Waze, Apple Maps, and Yandex.Maps.',
     appleWebApp: {
         title: 'Location Picker',
     },
     openGraph: {
-        title: 'Location Picker - Search, Convert, and Share Locations',
-        description: 'Location Picker is a free online tool for searching, converting, and sharing locations by address or coordinates. Supports DD and DMS formats, and integration with Google Maps, Waze, Apple Maps, and Yandex.Maps.',
+        title: 'Location Picker - Store and Share Locations',
+        description: 'Location Picker is a free online tool for storing and sharing locations by address or coordinates. Supports DD and DMS formats, and integration with Google Maps, Waze, Apple Maps, and Yandex.Maps.',
         type: 'website',
-        locale: 'ru_RU',
+        locale: 'en_US',
         url: '/',
         siteName: 'Location Picker',
     },
     twitter: {
-        title: 'Location Picker - Search, Convert, and Share Locations',
-        description: 'Location Picker is a free online tool for searching, converting, and sharing locations by address or coordinates. Supports DD and DMS formats, and integration with Google Maps, Waze, Apple Maps, and Yandex.Maps.',
+        title: 'Location Picker - Store and Share Locations',
+        description: 'Location Picker is a free online tool for storing and sharing locations by address or coordinates. Supports DD and DMS formats, and integration with Google Maps, Waze, Apple Maps, and Yandex.Maps.',
         card: 'summary_large_image',
     },
 }
